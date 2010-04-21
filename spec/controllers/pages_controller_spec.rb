4c700a92e2b5d0730a6c6c3c98decef82b00a6b9
Richard Yu <ryu@blindmicemedia.com>
2010-04-21 16:11:24 -0700

    starting new branch with blueprint css and changes to layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,12 +3,10 @@ require 'spec_helper'
 describe PagesController do
   integrate_views
 
-  #Delete these examples and add some real ones
-  it "should use PagesController" do
-    controller.should be_an_instance_of(PagesController)
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
-
-
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -16,8 +14,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'home'
-      response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_tag("title", @base_title + " | Home")
     end
   end
 
@@ -28,8 +25,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_tag("title", @base_title + " | Contact")
     end
   end
   
@@ -40,8 +36,18 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'about'
-      response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | About")
+      response.should have_tag("title", @base_title + " | About")
+    end
+  end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'help'
+      response.should have_tag("title", @base_title + " | Help")
     end
   end
   
